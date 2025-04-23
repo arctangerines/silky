f7b4fe356866f51b84da4614fb962e8e9e6f355b
arctangerines <arctangerines@icloud.com>
2025-04-23 00:43:52 -0500

    Starting work on some utility functions

--- src/silky.c
+++ src/silky.c
@@ -1,12 +1,61 @@
 #include "silky.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wchar.h>
 
 // So this string library is supposed to be compatible with regular string
 // functions so we just wanna return the buffer but have a region that comes
 // before the string that contains meta data, classic malloc pattern
+// NOTE: I think this needs to be packed but not sure so i'm not going to do it
+__attribute__((packed)) struct silky
+{
+    size_t len;
+};
+typedef struct silky slk;
 
-struct silkydata;
+size_t
+slklen(wchar_t* string)
+{
+    wchar_t* strc;
+    /* So pretty clever pattern thats very common
+     * Since with pointer arithmetic you can do ++ or add + 1 and the pointer
+     * will move 1 n sized place (the n depends of type it points to)
+     * so if its a char itll move 1 byte if its a long long int, the pointer
+     * will move 8 bytes
+     * but if you subtract the ending pointer with the starting pointer
+     * it will not give you the bytes it moved,
+     * it will give you the number of PLACES it moved
+     */
+    for (strc = string; *strc != '\0'; strc++);
+    return strc - string;
+}
+
+// Implementation of silk mem copy
+//FIXME: OK maybe we do need another member saying how much of the array is
+// free
+void
+slkmemcpy(wchar_t* dest, wchar_t* src, size_t len)
+{
+    const wchar_t* srcstr = src;
+    // this doesnt work cuz we also need to increment the dest
+    // for (*dest = *srcstr; *srcstr != '\0'; srcstr++);
+    while (len--)
+    {
+        *dest++ = *srcstr++;
+    }
+}
+
+wchar_t*
+slkinit(wchar_t* string)
+{
+    size_t len   = slklen(string);
+    slk*   s     = malloc(sizeof(slk) + (sizeof(wchar_t) * (len + 1)));
+    s->len       = len;
+    wchar_t* str = (void*)(s + 1); // add then cast
+}
+
 int
 main()
 {
-    return 1;
 }
