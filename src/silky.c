6f34654b788c3f8900f7e77733aa7e8dee08baa5
arctangerines <arctangerines@icloud.com>
2025-04-23 14:21:53 -0500

    It's smart until it isn't, so saving it for future reference but this is going away

--- src/silky.c
+++ src/silky.c
@@ -38,8 +38,28 @@ void
 slkmemcpy(wchar_t* dest, wchar_t* src, size_t len)
 {
     const wchar_t* srcstr = src;
-    // this doesnt work cuz we also need to increment the dest
-    // for (*dest = *srcstr; *srcstr != '\0'; srcstr++);
+    // this is what we return since it maintains the starting value
+    // what we pass is a copy thonk
+    wchar_t* deststr = dest;
+
+    // destination string metadata
+    struct silky* destmd = (struct silky*)dest - 1;
+    // the new string is larger
+    // I dont like this sectiuon actually, makes things too complex
+    // this is bad we are removing this
+    if (len > destmd->len)
+    {
+        destmd =
+            realloc(destmd, sizeof(struct silky) + (sizeof(wchar_t) * len + 1));
+        dest = (wchar_t*)(destmd + 1);
+        deststr = dest;
+    }
+
+    // If we do it after, we are gonna be putting it at undefined behaviour
+    // the while(len--) stops at 0 but it subtracts 1 more time, so it wraps
+    deststr[len] = '\0';
+
+    // more elegant and easier to digest
     while (len--)
     {
         *dest++ = *srcstr++;
