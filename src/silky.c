48a7fd487e098d5707caa1267aba7b419384d5f2
arctangerines <arctangerines@icloud.com>
2025-04-23 15:30:18 -0500

    I don't even know whats happening - Started work on slkcat

--- src/silky.c
+++ src/silky.c
@@ -1,18 +1,40 @@
 #include "silky.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <wchar.h>
 
+// TODO: Function to convert from wchar_t to char and vice versa
+// TODO: Function to compare strings
+
+
+// TODO: Function to concatenate
+wchar_t*
+slkcat(wchar_t* str1, wchar_t* str2)
+{
+    // STEP: Get lens
+    // STEP: malloc a new silky+str adding the 2 lens
+    // STEP: memcpy(?) to the new str
+}
+
+enum chartype
+{
+    WCHAR,
+    CHAR,
+};
 // So this string library is supposed to be compatible with regular string
 // functions so we just wanna return the buffer but have a region that comes
 // before the string that contains meta data, classic malloc pattern
 // NOTE: I think this needs to be packed but not sure so i'm not going to do it
-__attribute__((packed)) struct silky
+struct silky
 {
+
     size_t len;
+    // Extra spaces in the alloc'd string
+    // functions to apply to this: slklen, it should update the value
+    // slkmemcpy
+    size_t extra;
 };
-typedef struct silky slk;
+
 
 size_t
 slklen(wchar_t* string)
@@ -32,50 +54,37 @@ slklen(wchar_t* string)
 }
 
 // Implementation of silk mem copy
-//FIXME: OK maybe we do need another member saying how much of the array is
+// FIXME: OK maybe we do need another member saying how much of the array is
 // free
-void
+wchar_t*
 slkmemcpy(wchar_t* dest, wchar_t* src, size_t len)
 {
+    wchar_t* deststr= dest;
     const wchar_t* srcstr = src;
-    // this is what we return since it maintains the starting value
-    // what we pass is a copy thonk
-    wchar_t* deststr = dest;
-
-    // destination string metadata
-    struct silky* destmd = (struct silky*)dest - 1;
-    // the new string is larger
-    // I dont like this sectiuon actually, makes things too complex
-    // this is bad we are removing this
-    if (len > destmd->len)
-    {
-        destmd =
-            realloc(destmd, sizeof(struct silky) + (sizeof(wchar_t) * len + 1));
-        dest = (wchar_t*)(destmd + 1);
-        deststr = dest;
-    }
-
-    // If we do it after, we are gonna be putting it at undefined behaviour
-    // the while(len--) stops at 0 but it subtracts 1 more time, so it wraps
-    deststr[len] = '\0';
 
     // more elegant and easier to digest
     while (len--)
     {
-        *dest++ = *srcstr++;
+        *deststr++ = *srcstr++;
     }
+    return dest;
 }
 
 wchar_t*
 slkinit(wchar_t* string)
 {
-    size_t len   = slklen(string);
-    slk*   s     = malloc(sizeof(slk) + (sizeof(wchar_t) * (len + 1)));
+    size_t        len = slklen(string);
+    struct silky* s =
+        malloc(sizeof(struct silky) + (sizeof(wchar_t) * (len + 1)));
     s->len       = len;
-    wchar_t* str = (void*)(s + 1); // add then cast
+    wchar_t* str = (wchar_t*)(s + 1); // add then cast
+    slkmemcpy(str, string, len);
+    return str;
 }
 
 int
 main()
 {
+    wchar_t* mystr = slkinit(L"heyo");
+    free((struct silky*)mystr - 1);
 }
